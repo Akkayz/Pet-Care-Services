504bf86efa8b97482f2a5a63696334fa97ba5022
khanh1133 <2124801030252@student.tdmu.edu.vn>
2023-10-23 13:17:00 +0700

    Dang Ky, Dang Nhap

---
+++ DichVuThuCungKVH/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DichVuThuCungKVH.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DangKy()
+        { return View(); }
+        public ActionResult DangNhap() 
+        { return View(); }
+    }
+}
