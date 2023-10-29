e740d8fdb703d1c9dc2c0a01a10c24ef0c986ce0
Akkay <nguyenhung.dev.info@gmail.com>
2023-10-29 19:12:25 +0700

    - Login page - QL khách hàng - QL nhân viên - QL quản lí - QL sản phẩm - QL tài khoản

---
+++ DichVuThuCungKVH/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿// HomeController.cs
+using DichVuThuCungKVH.Model;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace DichVuThuCungKVH.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DACSEntities db = new DACSEntities();
+
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string UserName, string Password)
+        {
+            TaiKhoan ad = db.TaiKhoans.SingleOrDefault(n => n.TenTK == UserName && n.MatKhau == Password && n.LoaiTK == "1");
+            if (ad != null)
+            {
+                Session["Admin"] = ad;
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                return View();
+            }
+        }
+    }
+}
--- DichVuThuCungKVH/Controllers/HomeController.cs
+++ DichVuThuCungKVH/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DichVuThuCungKVH.Model;
 
 namespace DichVuThuCungKVH.Controllers
 {
